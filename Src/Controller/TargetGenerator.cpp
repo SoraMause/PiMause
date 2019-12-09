897c91e385bd5b45c75e0c89734d5d572f216006
SoraMause <alcnemy71@gmail.com>
2019-12-09 11:19:56 +0900

    壁のゲイン調整中

--- Src/Controller/TargetGenerator.cpp
+++ Src/Controller/TargetGenerator.cpp
@@ -68,25 +68,25 @@ void TargetGenerator::calcStepFrequency( float velocity )
 int16_t TargetGenerator::clacSideSensorP(Sensor_Data& sen_left, Sensor_Data& sen_right, bool act, bool rotation)
 {
   float step_value = 0.0f;
-  if( act && !rotation && (store_velocity > 100.0f) && (sen_left.diff_3ms < 100 || sen_right.diff_3ms < 100) ){
+  if( act && !rotation && (store_velocity > 100.0f) && (sen_left.diff_3ms < 30 || sen_right.diff_3ms < 30) ){
     if(sen_left.now > sen_left.threshold && sen_right.now > sen_right.threshold){
       // 両壁があるとき
       step_value = (float)(sen_left.now-sen_left.reference) - (sen_right.now-sen_right.reference);
       step_value *= sensor_kp;
-      if(step_value > 200) step_value = 200.0f;
-      else if(step_value < -200) step_value = -200.0f;
+      if(step_value > 100) step_value = 100.0f;
+      else if(step_value < -100) step_value = -100.0f;
     } else if(sen_left.now > sen_left.threshold){
       // 左壁があるとき
       step_value = (float)(sen_left.now-sen_left.reference);
       step_value *= 2.0f * sensor_kp;
-      if(step_value > 200) step_value = 200.0f;
-      else if(step_value < -50) step_value = -200.0f;
+      if(step_value > 100) step_value = 100.0f;
+      else if(step_value < -100) step_value = -100.0f;
     } else if(sen_right.now > sen_right.threshold){
       // 右壁があるとき
       step_value = (float)(sen_right.now-sen_right.reference);
       step_value *= -2.0f * sensor_kp;
-      if(step_value > 200) step_value = 200.0f;
-      else if(step_value < -200) step_value = -200.0f;
+      if(step_value > 100) step_value = 100.0f;
+      else if(step_value < -100) step_value = -100.0f;
     } else {
       step_value = 0.0f;
     } 
