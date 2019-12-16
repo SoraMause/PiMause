02477b7608daf2a7261af4d5feed483801c8cde1
SoraMause <alcnemy71@gmail.com>
2019-12-16 11:45:46 +0900

    壁制御の閾値設定

--- Src/Controller/TargetGenerator.cpp
+++ Src/Controller/TargetGenerator.cpp
@@ -69,19 +69,19 @@ int16_t TargetGenerator::clacSideSensorP(Sensor_Data& sen_left, Sensor_Data& sen
 {
   float step_value = 0.0f;
   if( act && !rotation && (store_velocity > 100.0f) ){
-    if(sen_left.now > sen_left.threshold && sen_right.now > sen_right.threshold && sen_left.diff_1ms < 30 && sen_right.diff_1ms < 30){
+    if(sen_left.now > sen_left.threshold && sen_right.now > sen_right.threshold && sen_left.diff_1ms < 20 && sen_right.diff_1ms < 20){
       // 両壁があるとき
       step_value = (float)(sen_left.now-sen_left.reference) - (sen_right.now-sen_right.reference);
       step_value *= sensor_kp;
       if(step_value > 100) step_value = 100.0f;
       else if(step_value < -100) step_value = -100.0f;
-    } else if(sen_left.now > sen_left.threshold && sen_left.diff_1ms < 15 ){
+    } else if(sen_left.now > sen_left.threshold && sen_left.diff_1ms < 7 ){
       // 左壁があるとき
       step_value = (float)(sen_left.now-sen_left.reference);
       step_value *= 2.0f * sensor_kp;
       if(step_value > 100) step_value = 100.0f;
       else if(step_value < -100) step_value = -100.0f;
-    } else if(sen_right.now > sen_right.threshold && sen_right.diff_1ms < 15 ){
+    } else if(sen_right.now > sen_right.threshold && sen_right.diff_1ms < 7 ){
       // 右壁があるとき
       step_value = (float)(sen_right.now-sen_right.reference);
       step_value *= -2.0f * sensor_kp;
