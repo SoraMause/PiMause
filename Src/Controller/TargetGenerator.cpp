498a84c53d6c05a7ef1da4d4f5ef1f22ee2d748e
SoraMause <alcnemy71@gmail.com>
2019-12-17 10:23:33 +0900

    sensorの値調整

--- Src/Controller/TargetGenerator.cpp
+++ Src/Controller/TargetGenerator.cpp
@@ -70,7 +70,7 @@ int16_t TargetGenerator::clacSideSensorP(Sensor_Data& sen_left, Sensor_Data& sen
   float step_value = 0.0f;
   float step_d = 0.0f;
   if( act && !rotation && (store_velocity > 100.0f) ){
-    if(sen_left.now > sen_left.threshold && sen_right.now > sen_right.threshold && sen_left.diff_3ms < 20 && sen_right.diff_3ms < 20){
+    if(sen_left.now > sen_left.threshold && sen_right.now > sen_right.threshold && sen_left.diff_1ms < 20 && sen_right.diff_1ms < 20){
       // 両壁があるとき
       step_value = (float)(sen_left.now-sen_left.reference) - (sen_right.now-sen_right.reference);
       step_d = (step_value - step_diff) * sensor_kd;
@@ -79,14 +79,14 @@ int16_t TargetGenerator::clacSideSensorP(Sensor_Data& sen_left, Sensor_Data& sen
       step_diff = step_value;
       if(step_value > 100) step_value = 100.0f;
       else if(step_value < -100) step_value = -100.0f;
-    } else if(sen_left.now > sen_left.threshold && sen_left.diff_3ms < 7 ){
+    } else if(sen_left.now > sen_left.threshold && sen_left.diff_1ms < 7 ){
       // 左壁があるとき
       step_value = (float)(sen_left.now-sen_left.reference);
       step_value *= 2.0 * sensor_kp;
       step_diff = step_value;
       if(step_value > 100) step_value = 100.0f;
       else if(step_value < -100) step_value = -100.0f;
-    } else if(sen_right.now > sen_right.threshold && sen_right.diff_3ms < 7 ){
+    } else if(sen_right.now > sen_right.threshold && sen_right.diff_1ms < 7 ){
       // 右壁があるとき
       step_value = (float)(sen_right.now-sen_right.reference);
       step_value *= -2.0 * sensor_kp;
