418563eb18561cbf71d415f6f875d2104ec7eb78
YutaSeya <bp17069@shibaura-it.ac.jp>
2020-01-31 10:54:46 +0900

    壁制御の入力がミスってた

--- Src/Controller/TargetGenerator.cpp
+++ Src/Controller/TargetGenerator.cpp
@@ -86,7 +86,7 @@ int16_t TargetGenerator::clacSideSensorP(Sensor_Data& sen_left, Sensor_Data& sen
       else if(step_value < -100) step_value = -100.0f;
     } else if(sen_right.now > sen_right.threshold && sen_right.diff_1ms < 10 && sen_left.diff_1ms < 20 ){
       // 右壁があるとき
-      step_value = (float) sensor_kp *(sen_right.now-sen_right.reference);
+      step_value = (float)-sensor_kp *(sen_right.now-sen_right.reference);
       step_diff = step_value;
       if(step_value > 100) step_value = 100.0f;
       else if(step_value < -100) step_value = -100.0f;
